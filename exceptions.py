6dd6d30f8ebf6c3295d2349c6c0e858d8cf4f4f5
SuperTapood <yoav.o252@gmail.com>
2021-01-14 12:40:54 +0200

    added custom exceptions

---
+++ exceptions.py
@@ -0,0 +1,43 @@
+class OverrideError(Exception):
+    # raised when a method cannot be overriden
+    # useful for users who didn't spend a million billion
+    # hours developing this API
+    # kinda useful
+
+    __module__ = Exception.__module__
+
+    def __init__(self, method, class_name):
+        self.method = method
+        self.class_name = class_name
+        return
+
+    def __str__(self):
+        return f"method '{self.method}' does not exist in class '{self.class_name}'"
+
+    pass
+
+
+class BotNotNamed(Exception):
+    # raised when a bot isn't given a name
+
+    __module__ = Exception.__module__
+
+    def __str__(self):
+        return "Framework can not access the name of this bot"
+
+    pass
+
+
+class NoTokenFound(Exception):
+    # raised when the framework can't find the token
+
+    __module__ = Exception.__module__
+
+    def __init__(self, name):
+        self.name = name
+        return
+
+    def __str__(self):
+        return f"cannot find token file - {self.name}.token"
+
+    pass
