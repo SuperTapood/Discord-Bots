e77af525c494f1fcc1d5d24ef66441ca25bfaf13
SuperTapood <yoav.o252@gmail.com>
2021-03-06 16:10:37 +0200

    kinda updated

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Discord-Bots
 
 todo:
+- fix issue where the bot can't recognize
+  user after reading from json file
 
 - tidy and order functions
 - match stubs and original functions
--- uncle/uncleCog.py
+++ uncle/uncleCog.py
@@ -11,8 +11,8 @@ class UncleCog(MasterCog):
     path = "uncle/data.json"
 
     def __init__(self, bot):
-        self.data = {}
         super().__init__(bot)
+        self.data = self.read_data()
         return
 
     def read_data(self):
@@ -39,13 +39,13 @@ class UncleCog(MasterCog):
         return {
             "money": 100,
             "dps": 0,
-            "levels": [0],
+            "levels": [0, 0, 0, 0, 0, 0],
             "last": time()
         }
 
     @command(name="init")
     async def init(self, ctx):
-        self.data[ctx.author.id] = self.get_new_data()
+        self.data[str(ctx.author.id)] = self.get_new_data()
         self.write_data()
         await self.bot.send(ctx.channel, f"created profile for user {ctx.author.mention}")
         return
@@ -76,12 +76,43 @@ class UncleCog(MasterCog):
     @command(name="stats", aliases=["userstats", "stat"])
     async def stats(self, ctx):
         if await self.assert_user(ctx):
-            data = self.data[ctx.author.id]
+            data = self.data[str(ctx.author.id)]
             fields = [
                 (key, data[key], False) for key in data
             ]
             await self.bot.generate_send_embed(f"{ctx.author.display_name} stats",
                                                fields, ctx.channel)
+        return
+
+    def get_level(self, ctx, index):
+        return self.data[ctx.author.id]["levels"][index]
+
+    def calculate_price(self, ctx, index):
+        # i need to have some formula here
+        return 0
+
+    @command(name="browse")
+    async def browse(self, ctx):
+        if await self.assert_user(ctx):
+            fields = [
+                (f"Mine {i}, Level {self.get_level(ctx, i)} -> {self.get_level(ctx, i)}",
+                 f"{self.calculate_price(ctx, i)}$",
+                 i % 2 == 1)
+                for i in range(len(self.data[ctx.author.id]["levels"]))
+            ]
+            await self.bot.generate_send_embed(f"Price catalog for {ctx.author.display_name}", fields, ctx.channel)
+        return
+
+    @command(name="data")
+    async def get_data(self, ctx):
+        if ctx.author.id in Data.get_owners():
+            for player_id in self.data:
+                info = self.data[player_id]
+                fields = [
+                    (key, f"{type(value)}: {value}", False)
+                    for key, value in zip(info.keys(), info.values())
+                ]
+                await self.bot.generate_send_embed(f"{player_id} {type(player_id)} data", fields, ctx.channel)
         return
 
     pass
